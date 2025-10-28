8d3c6ad1b29b21d8ce8c1a74679cd41239c5f3c6
meyer30 <2603221+meyer30@users.noreply.github.com>
2025-10-28 13:37:14 -0500

    removing old code

---
+++ WinFormsApp1/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using static WinFormsApp1.Constants;
+
+namespace Models
+{
+
+
+    /// <summary>
+    /// Represents a Card in the 52 card deck.
+    /// </summary>
+    public class Card
+    {
+        public SuitEnum mySuit2 { get; set; }
+        public RankEnum myRank2 { get; set; }
+
+        public Card(SuitEnum suit, RankEnum value)
+        {
+            this.mySuit2 = suit;
+            this.myRank2 = value;
+        }
+
+        public int ZeroBaseRank => (int)myRank2 - 2;
+    };
+};
